c184914b1524717a7b9194345cd148fd9fc72b0d
M Shaharyar Nasir <mshaharyarnasir@gmail.com>
2022-01-03 03:46:02 +0500

    build

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import { RecoilRoot } from "recoil";
@@ -8,8 +7,6 @@ export default function App({
   pageProps: { session, ...pageProps },
 }) {
   return (
-    // `session` comes from `getServerSideProps` or `getInitialProps`.
-    // Avoids flickering/session loading on first load.
     <SessionProvider session={session}>
       <RecoilRoot>
         <Component {...pageProps} />
