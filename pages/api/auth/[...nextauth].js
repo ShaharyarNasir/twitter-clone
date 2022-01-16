31104ed234458f84cfb344bb8e54aa9dbdc9012a
M Shaharyar Nasir <mshaharyarnasir@gmail.com>
2022-01-16 17:54:43 +0500

    build

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,19 +2,25 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export default NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        }),
-    ],
-    callbacks: {
-        async session({ session, token }) {
-            session.user.tag = session.user.name.split(" ").join("").toLocaleLowerCase();
-            session.user.uid = token.sub;
-            return session;
-        },
-    },
+   providers: [
+      GoogleProvider({
+         clientId: process.env.GOOGLE_CLIENT_ID,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+   ],
+   secret: process.env.SECRET,
+   pages: {
+      signIn: "/auth/signin",
+   },
+   callbacks: {
+      async session({ session, token, user }) {
+         session.user.username = session.user.name
+            .split(" ")
+            .join("-")
+            .toLocaleLowerCase();
 
-    secret: process.env.JWT_SECRET,
+         session.user.uid = token.sub;
+         return session;
+      },
+   },
 });
